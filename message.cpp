f2e008b45f8020707313630d2912e0cc67a9d791
jacktorpas <85330975+jacktorpas@users.noreply.github.com>
2021-06-17 18:33:32 +0800

    Update message.cpp

--- message.cpp
+++ message.cpp
@@ -4,21 +4,28 @@
 using namespace std;
 
 void openingMessage(){
-    cout << "What Would You Do?";
-    cout << "\n----------------------------\n";
-    cout << "A simple console-based game that is played by entering simple verb-noun\n";
-    cout << "commands. Depending on what you choose to do, the story will have a\n";
-    cout << "different ending.\n\n";
-    cout << "Coded by Justin; story by Ec and Gel, and QA-tested by Charles :)\n\n"; //change this soon
-    cout << "Press ENTER to start...\n";
+    cout << "The Mysterious Case of Elisa Lam";
+    cout << "\n---------------------------------\n";
+    cout << "A console-based mystery/thriller game that is played by entering simple verb-noun commands. Depending on what\n";
+    cout << "you choose to do, the story will have a different ending.\n\n";
+    cout << "Coded by Justin; story by Ec, Gel, and Charles :)\n\n";
+    cout << "Press ENTER to continue...\n";
     cin.get();
 }
 
-void anotherMsg(){
-    cout << "Before we get started:\n";
-    cout << "\"What Would You Do?\" is an exploration game that takes us on a tour of\n";
-    cout << "protagonist Elisa Lam\'s solo trip getaway around the West Coast,\n";
-    cout << "as she resides in the hotel.\n\n";
-    cout << "Press ENTER again...\n";
+void warning(){
+    cout << "WARNING:\n";
+    cout << "You'll be playing as an LAPD detective investigating this case. This is based on a true yet mysterious\n";
+    cout << "story; possible gore and unsettling stuff are to be expected. As they say, \"viewer discretion is advised.\"\n";
+    cout << "If you are ready to play, press ENTER again...\n";
+    cin.get();
+}
+
+void closingMessage(){
+    cout << "Thank you for playing \"The Mysterious Case of Elisa Lam\". If that's not the ending you want, you may\n";
+    cout << "want to replay it and try different approaches. Your text inputs have been stored in inputs.txt and tracker.txt tells\n";
+    cout << "how many times you've played this story.\n\n";
+    cout << "CREDITS:\n\nJustin - Code\nEc, Gel, and Charles - Story\n";
+    cout << "Press ENTER to exit...\n";
     cin.get();
 }
