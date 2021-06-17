515dfbb22ce690bee3a2ce8bba47ce95fc3067e2
jacktorpas <85330975+jacktorpas@users.noreply.github.com>
2021-06-17 18:29:09 +0800

    Update chapters.h

--- chapters.h
+++ chapters.h
@@ -9,6 +9,51 @@ void chapterThree();
 void chapterFour();
 void chapterFive();
 
+//STORY 1
+void _1ChapterSix();
+void _1ChapterSeven();
+void _1ChapterEight();
+void _1Chapter9A();
+void _1Chapter9B();
+void _1ChapterN(std::string location);
+void _1Chapter10();
+void _1Chapter11();
+void _1Chapter12A();
+void _1Chapter12AA();
+void _1Chapter12B();
+
+//STORY 2
+void _2ChapterSix();
+void _2ChapterSeven();
+void _2Chapter8B();
+void _2ChapterN(std::string location);
+void _2Chapter10();
+void _2Chapter11AA();
+void _2Chapter11B();
+
+//CLIMAX AND ENDING
+void _1Chapter13();
+void _1Chapter14();
+void _1Chapter15A();
+void _1Chapter15B();
+void _1Chapter15C();
+int _1Chapter16A();
+void _1Chapter16B();
+void _1Chapter16C1();
+void _1Chapter16C2();
+void _1Chapter17A1A();
+void _1Chapter17A1B();
+void _1Chapter17A2A();
+void _1Chapter17A2B();
+void _1Chapter17B();
+void _1Chapter18A1();
+void _1Chapter18A2();
+void dialNums();
+void _1Chapter18B1A();
+void _1Chapter18B1B();
+void _1Chapter18B2A();
+void _1Chapter18B2B();
+
 //------------------------------------
 
 #endif
